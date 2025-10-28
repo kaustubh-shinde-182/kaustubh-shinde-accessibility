f33daa021b0762e067a9a45442ea397b650cb159
Kaustubh-shinde-182 <kaustubhshinde182@gmail.com>
2025-10-28 13:56:30 +0530

    added setupTest.ts file as per the vite.config documentation

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import "@testing-library/jest-dom";
 
 describe("App", () => {
   test("renders string calculator heading", () => {
---
+++ src/setupTests.ts
@@ -0,0 +1,5 @@
+import "@testing-library/jest-dom";
+import { expect } from "vitest";
+
+// Optional: Extend Vitest expectations with jest-dom matchers
+// This happens automatically with @testing-library/jest-dom
