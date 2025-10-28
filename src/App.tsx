48d39109c57c4ec3fa81533a47507be91462b11e
Kaustubh-shinde-182 <kaustubhshinde182@gmail.com>
2025-10-28 14:01:26 +0530

    fixed alt text to image for screen readers

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const App = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [result] = useState(null);
 
   const handleCalculate = () => {};
 
   return (
-    <div style={{ padding: '20px', backgroundColor: '#fff', color: '#aaa' }}>
+    <div style={{ padding: "20px", backgroundColor: "#fff", color: "#aaa" }}>
       <img
-        src='https://images.unsplash.com/photo-1594352161389-11756265d1b5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        src="https://images.unsplash.com/photo-1594352161389-11756265d1b5?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         width={600}
         height={400}
+        alt="Beautiful landscape with mountains and lake"
       />
 
       <h2>String Calculator</h2>
 
-      <h1 style={{ fontSize: '20px' }}>Enter numbers</h1>
+      <h1 style={{ fontSize: "20px" }}>Enter numbers</h1>
 
       <textarea
-        style={{ margin: '10px 0', color: '#aaa' }}
-        placeholder='Enter numbers'
+        style={{ margin: "10px 0", color: "#aaa" }}
+        placeholder="Enter numbers"
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
@@ -28,17 +29,18 @@ const App = () => {
       <div
         onClick={handleCalculate}
         style={{
-          padding: '10px',
-          backgroundColor: '#008cba',
-          color: '#fff',
-          border: 'none',
-        }}>
+          padding: "10px",
+          backgroundColor: "#008cba",
+          color: "#fff",
+          border: "none",
+        }}
+      >
         Calculate
       </div>
 
-      {result !== null && <p style={{ color: 'green' }}>Result: {result}</p>}
+      {result !== null && <p style={{ color: "green" }}>Result: {result}</p>}
 
-      <div role='alert'>
+      <div role="alert">
         <p>Make sure you enter numbers correctly!</p>
       </div>
     </div>
